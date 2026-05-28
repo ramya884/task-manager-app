473fd21772618412adcf19bd2a592537055d2c01
ramya <lakshmisreeramya218@gmail.com>
2026-05-28 08:45:10 +0530

    fixed cors

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,9 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
